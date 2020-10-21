96c7228dda7b79fc9308bc6bdcf4f4b72777c103
tarz2929 <saitarunmulley@gmail.com>
2020-10-21 09:30:06 -0600

    First commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        // Public items can be seen anywhere.
+        // Auto-Implemented Property (Automatically generates a default "getter" (accessor) and "setter" (mutator).
+        // AccessModifier DataType PropertyName { get; set; }
+        public string FirstName { get; set; }
+
+        // Private items cannot be seen outside of the class.
+        private string _lastName;
+        // Fully-Implemented Property (You specify the beahviour for the "getter" (accessor) and "setter" (mutator).
+        public string LastName
+        {
+            get
+            {
+                // For the getter, typically you will want to return the value from the backing variable.
+                // You may format it first, etc. but that is not necessary.
+                return _lastName;
+            }
+            set
+            {
+                // For the setter, typically you will want to assign the backing variable to the keyword "value" which represents what is being assigned to the property.
+                // You may perform validation here as well.
+                _lastName = value;
+            }
+        }
+    }
+}
